8dd1ab15075d166371787247deb4267fb2771396
Wilfreschu <Wschuchner@udesa.edu.ar>
2024-11-15 18:12:48 -0300

    estaba mal el index

--- js/index.js
+++ js/index.js
@@ -2,6 +2,19 @@ let form = document.querySelector(".formu");
 let comentario = document.querySelector(".comentario");
 let errorMessage = document.querySelector(".error-message");
 
+fetch('https://dummyjson.com/recipes')
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data);
+    
+})
+.catch(function(error){
+    console.log('El error es: '+ error);
+})
+
+
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 
