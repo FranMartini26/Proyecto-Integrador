96107c4577a5fca1e5fbc5d12e60dcb2dd3b89a8
FranMartini <fmartini@udesa.edu.ar>
2024-11-29 11:08:57 -0300

    Puse en la misma linea la dificultad de las recetas en index

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ fetch('https://dummyjson.com/recipes')
                 <img src="${receta.image}" alt="">
                 <div class="textos">
                     <p class="nombre">${receta.name}</p>
-                    <p class ="hola">Nivel de dificultad: ${receta.difficulty}</p>
+                    <p>Nivel de dificultad: ${receta.difficulty}</p>
                 </div>
                 </a>
             </article>
@@ -51,14 +51,6 @@ fetch('https://dummyjson.com/recipes')
         let newContainer = document.querySelector(".segundocontenedor");
         let boton = document.querySelector(".carga");
         boton.style.backgroundColor = "beige"; 
-        
-
-        let dificultad = document.querySelector(".hola")
-        for (i=0;i<dificultad.length; i++){
-            dificultad.style.display = "flex"
-            dificultad.style.justifyContent = "flex-start"
-            dificultad.style.alignItems = "flex-end"
-        }
         
         
         boton.addEventListener("mouseover", function() {
