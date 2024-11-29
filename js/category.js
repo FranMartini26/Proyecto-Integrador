61dd5120e836aa15f1a57d80063035d92719d9b7
Zembo <jzemborain@udesa.edu.ar>
2024-11-29 15:36:35 -0300

    Cambios hechos en clase

--- js/category.js
+++ js/category.js
@@ -9,69 +9,82 @@ fetch(`https://dummyjson.com/recipes/tag/${tag}`)
         return response.json()
     })
     .then(function(data){
-        console.log(data.recipes)
-        let grid = document.querySelector(".seccion-invididual");
+
+        document.querySelector(".nombre").innerText = `${tag}`
+
+        let grid = document.querySelector(".seccion-individual");
         let recetas = "";
 
         for(let i = 0; i < data.recipes.length; i++){
             let receta = data.recipes[i]
             let article = `
-                        <article class="articles">
+                        <article class="articles123">
                             <a href="receta.html?id=${receta.id}" class="link-detalle">
-                            <img class="imagen" src= ${receta.image} alt=''>
-                            <p class="titulo">${tag}</p>
-                            <div class="textos">
-                            <p>Título: ${receta.name} </p>
-                            <p>Nivel de dificultad: ${receta.difficulty} </p>
-                            </div>
-                        </a>
+                            <img src= ${receta.image} alt=''>
+                            <p class="titu">Título: ${receta.name} </p>
+                            <p class="difi">Nivel de dificultad: ${receta.difficulty} </p>
+                            </a>
                         </article>
                              `
             recetas += article
         }
 
         grid.innerHTML = recetas
-        let articulo = document.querySelector(".articles")
-        articulo.style.backgroundColor = "beige"
-        articulo.style.height = "500px"
-        let titulo = document.querySelector(".titulo")
-        titulo.style.fontWeight = "bold"
-        titulo.style.fontSize = "30px"
-        titulo.style.marginLeft = "50px"
-        titulo.style.color = "black";
-        let imagen = document.querySelectorAll(".imagen")
+
+        grid.style.display = "flex"
+        grid.style.alignItems = "center"
+        grid.style.flexWrap = "wrap"
+        grid.style.justifyContent = "space-around"
+
+        let tit = document.querySelectorAll('.titu')
+        for(let i=0; i < tit.length; i++){
+            let titu = tit[i]
+            titu.style.paddingTop = "12px"
+        }
+
+        let dif = document.querySelectorAll('.difi')
+        for(let i=0; i < dif.length; i++){
+            let difi = dif[i]
+            difi.style.paddingTop = "12px"
+        }
+
+
+        let imagen = document.querySelectorAll(".seccion-individual img")
         for(let i = 0; i < imagen.length; i++){
             let img = imagen[i]
-            img.style.width = "100%"
+            img.style.width = "450px"
             img.style.borderRadius = "5px"
         }
 
-        let articles = document.querySelectorAll(".articles")
+        let articles = document.querySelectorAll(".articles123")
         for(i = 0; i < articles.length; i++){
             let art = articles[i]
             art.style.border = "solid lightgrey"
-            art.style.padding = "7px"
-            art.style.margin = "25px"
+            art.style.padding = "15px"
+            art.style.margin = "50px"
             art.style.borderRadius = "7px"
+            art.style.backgroundColor = "beige"
+            art.style.height = "540px"
+
             art.addEventListener("mouseover", function() {
                 art.style.backgroundColor = "lightgreen";
             });
+    
             art.addEventListener("mouseout", function() {
                 art.style.backgroundColor = "beige";
             });
-
         }
 
         let nombre = document.querySelector(".nombre")
         nombre.style.textAlign = "center"
         nombre.style.paddingTop = "10px"
-        nombre.style.fontSize = "35px"
+        nombre.style.fontSize = "50px"
 
         let detalle = document.querySelectorAll(".link-detalle")
         for(i = 0; i < detalle.length; i++){
         let det = detalle[i]
         det.style.textDecoration = "none"
-        det.style.color = "green"
+        det.style.color = "black"
         }
 
     })
